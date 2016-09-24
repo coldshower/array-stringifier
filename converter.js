79af099a9fb5168d05c3c1532d2e780b4778eaaf
John Chen <john@johnchen.co>
2016-09-24 01:02:25 -0400

    stringify

--- converter.js
+++ converter.js
@@ -1,12 +1,14 @@
 var Converter = {
 	encode: function (array) {
 		if (Array.isArray(array)) {
-			var index = 0;
-			var result = '';
-			array.forEach(function (elem) {
-				result += (index + '%%' + elem);
-				index += 1;
-			});
+			var result = '[';
+			for (var i = 0; i < array.length; i++) {
+				result += wrap(escapeQuotes(array[i]));
+				if (i < array.length - 1) {
+					result += ',';
+				}
+			}
+			result += ']';
 			return result;
 		} else {
 			throw new Error('encode only takes an array argument');
@@ -16,7 +18,7 @@ var Converter = {
 	decode: function (string) {
 		if (typeof string === 'string') {
 			var result = [];
-			for (var i = 0; i < string.length; i++) {
+			for (var i = 1; i < string.length; i++) {
 				
 			}
 			return result;
@@ -25,3 +27,36 @@ var Converter = {
 		}
 	}
 };
+
+function wrap(string) {
+	return '"' + string + '"';
+}
+
+function escapeQuotes(string) {
+	return string.replace(/"/g, '\\"');
+}
+
+function unwrap(str) {
+	return str.slice(1, -2);
+}
+
+function parse(string) {
+	var result = [];
+	var current = [];
+	var currentlyInString = false;
+	for (var i = 1; i < string.length - 1; i++) {
+		if (string[i] === '"' && !currentlyInString) {
+			currentlyInString = true;
+		} else {
+			if (string[i] === ',' && string[i + 1] === '"') {
+				result.push(current.join(''));
+				current = [];
+				currentlyInString = false;
+			} else {
+				current.push(string[i]);
+			}
+		}
+		
+	}
+	return result;
+}
